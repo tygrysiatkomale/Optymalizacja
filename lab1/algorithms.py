f9743d4bd645c7b88cd1b0e1535d9cf983790979
oliwiaaa333 <wiatrowskaoliwiaxxx@gmail.com>
2024-10-29 16:57:15 +0100

    dodanie definicji metod i klasy fcalls

--- lab1/algorithms.py
+++ lab1/algorithms.py
@@ -1,1 +1,109 @@
 # plik z algorytmami - ekspansja, lagrange, fibonacci
+
+class FCallsUnique:
+    def __init__(self, func):
+        self.func = func
+        self.calls = 0
+
+    def __call__(self, x):
+        self.calls += 1
+        return self.func(x)
+
+# metoda ekspansji
+def expansion_method(f, x_0, d, alfa, n_max=1000):
+    f = FCallsUnique(f)
+    i = 0
+    x_1 = x_0 + d
+    x = [x_0, x_1]
+
+    if f(x[1]) == f(x[0]):
+        return x[0], x[1], f.calls
+
+    if f(x[1]) > f(x[0]):
+        d = -d
+        x[1] = x[0] + d
+        if f(x[1]) >= f(x[0]):
+            return x[1], (x[0] - d), f.calls
+
+    while True:
+        if f.calls > n_max:
+            raise ValueError("Error: Przekroczono N_MAX")
+        i = i + 1
+        x.append(x[0] + (alfa ** i) * d)
+
+        if f(x[i]) <= f(x[i + 1]):
+            break
+
+    if d > 0:
+        return x[i - 1], x[i + 1], f.calls
+
+    return x[i + 1], x[i - 1], f.calls
+
+# metoda fibonacciego
+def fibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n + 1):
+        fib.append(fib[-1] + fib[-2])
+    return fib
+
+
+def fibonacci_method(f, a, b, epsilon):
+    f = FCallsUnique(f)
+    n = 1
+    fib = fibonacci(100)
+
+    while fib[n] < (b - a) / epsilon:
+        n += 1
+
+    k = n
+    a_i = a
+    b_i = b
+
+    c_i = b_i - (fib[k - 1] / fib[k]) * (b_i - a_i)
+    d_i = a_i + b_i - c_i
+
+    for i in range(k - 2):
+        if f(c_i) < f(d_i):
+            b_i = d_i
+            d_i = c_i
+            c_i = b_i - (fib[k - i - 2] / fib[k - i - 1]) * (b_i - a_i)
+        else:
+            a_i = c_i
+            c_i = d_i
+            d_i = a_i + b_i - c_i
+
+    return (c_i + d_i) / 2, f.calls
+
+# metoda lagrange (bez fcalls)
+def lagrange_interpolation(func, a, b, c, epsilon=1e-5, gamma=1e-5, max_iter=100):
+    i = 0
+    while i < max_iter:
+        l = (func(a) * (b ** 2 - c ** 2) + func(b) * (c ** 2 - a ** 2) + func(c) * (a ** 2 - b ** 2))
+        m = (func(a) * (b - c) + func(b) * (c - a) + func(c) * (a - b))
+
+        if m <= 0:
+            raise ValueError("Błąd: podział przez zero lub ujemna wartość w metodzie Lagrange’a.")
+
+        d = 0.5 * l / m
+
+        if a < d < c:
+            if func(d) < func(c):
+                b = c
+                c = d
+            else:
+                a = d
+        elif c < d < b:
+            if func(d) < func(c):
+                a = c
+                c = d
+            else:
+                b = d
+        else:
+            raise ValueError("Błąd: punkt interpolacji d jest poza zakresem przedziału.")
+
+        if abs(b - a) < epsilon or abs(d - c) < gamma:
+            return d
+
+        i += 1
+
+    raise RuntimeError("Przekroczono maksymalną liczbę iteracji bez zbieżności.")
